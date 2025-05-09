9c426f763750312a1aaff9c6ef9ea533e6b12511
SudiCodes <eee.ee166480@silicon.ac.in>
2025-05-09 21:19:34 +0530

    cybagel1

--- l1.py
+++ l1.py
@@ -1,32 +1,179 @@
-# st = "Su78*{*dipta Sa^{^lmal"
+Cybage
+'''
 
-# res = ""
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
 
-# l = '123456789@#$%^&8{}*'
+'''
 
-# for i in st:
-#     if i.isalpha() or i == " ":
-#         res+=i 
-        
+
+# Sort the data by age
+
+persons = [
+
+	{
+
+	 "id": 1,
+
+	 "fname": "Albert",
+
+	 "age": 35
+
+	},
+
+	{
+
+	 "id": 2,
+
+	 "fname": "Issac",
+
+	 "age": 32
+
+	},
+
+	{
+
+	 "id": 3,
+
+	 "fname": "James",
+
+	 "age": 33
+
+	},
+
+	{
+
+	 "id": 4,
+
+	 "fname": "Edison",
+
+	 "age": 32
+
+	}
+
+]
+
+
+res =  sorted(persons,key = lambda x:x["age"])
 # print(res)
 
-li =[2,7,3,8,1,6,8]
-target = 9 
 
-temp_dic = {}
+# Given a list 
 
-res = []
-for i in li:
-    diff = target - i 
+persons = [
+
+    {
+
+        "id": 1,
+
+        "name": "Alice Johnson",
+
+        "gender": "Female",
+
+        "date_of_birth": "1985-07-23",
+
+        "salary": 75000,
+
+        "department": "Engineering"
+
+    },
+
+    {
+
+        "id": 2,
+
+        "name": "Bob Smith",
+
+        "gender": "Male",
+
+        "date_of_birth": "1978-11-15",
+
+        "salary": 68000,
+
+        "department": "Sales"
+
+    },
+
+    {
+
+        "id": 3,
+
+        "name": "Carol Williams",
+
+        "gender": "Female",
+
+        "date_of_birth": "1992-04-08",
+
+        "salary": 72000,
+
+        "department": "Engineering"
+
+    },
+
+    {
+
+        "id": 4,
+
+        "name": "David Brown",
+
+        "gender": "Male",
+
+        "date_of_birth": "1989-11-30",
+
+        "salary": 80000,
+
+        "department": "Sales"
+
+    },
+
+    {
+
+        "id": 5,
+
+        "name": "Emma Davis",
+
+        "gender": "Female",
+
+        "date_of_birth": "1975-12-20",
+
+        "salary": 90000,
+
+        "department": "Sales"
+
+    },
+
+    {
+
+        "id": 6,
+
+        "name": "Frank Miller",
+
+        "gender": "Male",
+
+        "date_of_birth": "1982-07-14",
+
+        "salary": 85000,
+
+        "department": "Operations"
+
+    }
+
+]
+
+# 1) Group together persons that have birthdays in the same month
+
+user_grp = {}
+
+for i in persons:
     
-    if diff not in temp_dic:
-        temp_dic[diff] = i 
+    if i["date_of_birth"].split("-")[1] not in user_grp:
+        user_grp[i["date_of_birth"].split("-")[1]] = []
     
-    
-    if diff in temp_dic:
-        res.append((diff,temp_dic[diff]))
+    user_grp[i["date_of_birth"].split("-")[1]].append(i["name"])
+
+print(user_grp)
         
+ 
 
-res = [if j in (sorted(temp_dic)[:3]) for j in res ]
-     
-print(res)
